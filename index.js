af5452513d7c8b25b1ea01d04ff73a6ad2c43967
Fernando Oliveira <Ferd.olvr@gmail.com>
2022-02-19 12:28:13 -0300

    first commit introduction javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+// const nome = "Jósé"
+// const endereco = "Rua K, 12"
+// const sexo = "M"
+// const idade = 27
+
+// let a = 2
+// a = "abc"
+
+
+// var c  = 2+3
+// c = 5
+// var d = "abcd"
+
+var linguagem = "JavaScript"
+console.log("Aprendendo "+ linguagem)
